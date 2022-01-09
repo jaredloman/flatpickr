23bf90d4972dbb7cff917b5a254f4232a5244a62
Jared Loman <jared@jaredloman.com>
2022-01-09 07:41:12 -1000

    Add Dist Files
    Add compiled files for inclusion in other projects

---
+++ dist/esm/utils/dom.js
@@ -0,0 +1,37 @@
+export function toggleClass(elem, className, bool) {
+    if (bool === true)
+        return elem.classList.add(className);
+    elem.classList.remove(className);
+}
+export function createElement(tag, className, content) {
+    var e = window.document.createElement(tag);
+    className = className || "";
+    content = content || "";
+    e.className = className;
+    if (content !== undefined)
+        e.textContent = content;
+    return e;
+}
+export function clearNode(node) {
+    while (node.firstChild)
+        node.removeChild(node.firstChild);
+}
+export function findParent(node, condition) {
+    if (condition(node))
+        return node;
+    else if (node.parentNode)
+        return findParent(node.parentNode, condition);
+    return undefined;
+}
+export function getEventTarget(event) {
+    try {
+        if (typeof event.composedPath === "function") {
+            var path = event.composedPath();
+            return path[0];
+        }
+        return event.target;
+    }
+    catch (error) {
+        return event.target;
+    }
+}
